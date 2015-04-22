9d47bf8ec97b1071c28e1d538fec66fb9577a5db
Yuom Theara <yuom.theara@gmail>
2015-04-22 11:29:26 +0700

    first commit

---
+++ package.js
@@ -0,0 +1,23 @@
+Package.describe({
+    name: 'theara:moneyjs',
+    version: '0.0.1',
+    // Brief, one-line summary of the package.
+    summary: 'Simple and tiny JavaScript library for realtime currency conversion and exchange rate calculation, from any currency, to any currency.',
+    // URL to the Git repository containing the source code for this package.
+    git: 'https://github.com/rabbittc/moneyjs.git',
+    // By default, Meteor will default to using README.md for documentation.
+    // To avoid submitting documentation, set this field to null.
+    documentation: 'README.md'
+});
+
+Package.onUse(function (api) {
+    api.versionsFrom('1.1.0.2');
+
+    api.addFiles('moneyjs.js');
+});
+
+Package.onTest(function (api) {
+    api.use('tinytest');
+    api.use('theara:moneyjs');
+    api.addFiles('moneyjs-tests.js');
+});
